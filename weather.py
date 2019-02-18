2e0fa4e84867037a5312edc14d489407bd5a6a73
Mohammed <mmn17@my.fsu.edu>
2019-02-18 13:55:36 -0500

    Updated file weather.py fixed url

--- weather.py
+++ weather.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/temperature', methods=['POST'])
 def temperature():
     zipcode = request.form['zip']
-    req = requests.get('api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=6ce765045581e99ca1b61b279625072d')
+    req = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=6ce765045581e99ca1b61b279625072d')
     json_object = req.json()
     temp_k = float(json_object['main']['temp'])
     temp_f = (temp_k - 273.15) * 1.8 + 32
